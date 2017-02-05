363e8e062c92e9059adf039f1b8f877e3305ea4d
Bar Goldinfeld <bargoldinfeld@gmail.com>
2017-02-05 15:12:02 +0200

    webpack aot configuration

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,5 @@
 // Export your package from here
-// e.g: export * from './my-package-files-under-src
+// e.g: export * from './my-package-files-under-src;
+
+// Export your dependencies as UMDs
+// e.g: export * from 'angular2-dep';
